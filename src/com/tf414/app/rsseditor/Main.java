88d2a88d4a82b2c764025183177cf453ee6724bb
XiaoHuiHui233 <1025184872@qq.com>
2019-08-29 09:40:04 +0800

    build project

---
+++ src/com/tf414/app/rsseditor/Main.java
@@ -0,0 +1,8 @@
+package com.tf414.app.rsseditor;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Starting...");
+		System.out.println("Started complately!");
+	}
+}
---
+++ src/com/tf414/app/rsseditor/kernal/package-info.java
@@ -0,0 +1,1 @@
+package com.tf414.app.rsseditor.kernal;
